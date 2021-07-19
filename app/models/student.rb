48932f0c72ccff668889f6a399a4ef0862440322
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-07-18 20:10:29 -0600

    commit tree f5279db4f0435c6ad3e30086b5f30b9308a49126

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,17 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    #if query
+      student = Student.where("NAME like ?", "%#{query}%")   #need to figure out how to search parts of a word
+      if student
+        student
+      else
+        @students = Student.all
+      end
+    #else
+    #  @students = Student.all
+    #end
+  end
 end
